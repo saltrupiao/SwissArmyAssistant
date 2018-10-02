1788b675b164432c94a13460e7c95f6f57cc8f24
rakazirut <kaz.brah@gmail.com>
2018-10-02 12:29:04 -0400

    CalcClass.py

--- CalcClass.py
+++ CalcClass.py
@@ -9,13 +9,13 @@ class CalcClass(object):
             eval(inp)
         except ZeroDivisionError:  # Ex: 8/0
             result = 'Error: Divide by Zero'
-            return render_template('calc.html', result=result)
+            return result
         except NameError:  # Ex: a+1
             result = 'Error: Check your syntax'
-            return render_template('calc.html', result=result)
+            return result
         except SyntaxError:  # Ex: 3a+1, 3-+
             result = 'Error: Check your syntax'
-            return render_template('calc.html', result=result)
+            return result
 
         result = eval(inp)  # builds string result from the evaluation of user input expression
 
