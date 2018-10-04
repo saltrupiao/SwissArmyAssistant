0ba4be0391c884c03b7ba37675a57609ebad4350
rakazirut <kaz.brah@gmail.com>
2018-10-04 12:10:37 -0400

    Clean Branch

--- main.py
+++ main.py
@@ -25,26 +25,7 @@ def calcpage():
 @app.route('/calc', methods=['POST'])
 def calc():
     inp = request.form['display']  # pull expression from text field
-
     result = CalcClass.calculation(inp)
-
-    try:  # Data validation, will prevent program crash if user enters invalid expression
-        eval(inp)
-    except ZeroDivisionError:  # Ex: 8/0
-        result = 'Error: Divide by Zero'
-        return render_template('calc.html', result=result)
-    except NameError:  # Ex: a+1
-        result = 'Error: Check your syntax'
-        return render_template('calc.html', result=result)
-    except SyntaxError:  # Ex: 3a+1, 3-+
-        result = 'Error: Check your syntax'
-        return render_template('calc.html', result=result)
-
-    result = eval(inp)  # builds string result from the evaluation of user input expression
-
-    if isinstance(result, float):
-        result = round(result, 3)
-
     return render_template('calc.html', result=result)  # sends result to page
 
 
@@ -70,14 +51,9 @@ def noteFunctions():
         NoteClass.saveNote(filename, data)
         return render_template('note.html', text=data, fn=filename)
     if ans == "Load":
-<<<<<<< HEAD
         data = NoteClass.loadNote(filename)
         return render_template('note.html', text=data, fn=filename)
 
-=======
-        data = NoteClass.loadNote()
-        return render_template('note.html', text=data)
->>>>>>> 53fbbde85f5f7f5d97b0e427baf9b5202632a657
 
 if __name__ == '__main__':
     app.run(debug=True) # so the page refreshes live and doesn't need to be restarted
