550a6a14aa4169ee40ebf404f095828c0f201dcf
Robert Kazirut <rakazirut@oakland.edu>
2018-10-04 12:03:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,26 +25,7 @@ def calcpage():
 @app.route('/calc', methods=['POST'])
 def calc():
     inp = request.form['display']  # pull expression from text field
-
     result = CalcClass.calculation(inp)
-
-    try:  # Data validation, will prevent program crash if user enters invalid expression
-        eval(inp)
-    except ZeroDivisionError:  # Ex: 8/0
-        result = 'Error: Divide by Zero'
-        return render_template('calc.html', result=result)
-    except NameError:  # Ex: a+1
-        result = 'Error: Check your syntax'
-        return render_template('calc.html', result=result)
-    except SyntaxError:  # Ex: 3a+1, 3-+
-        result = 'Error: Check your syntax'
-        return render_template('calc.html', result=result)
-
-    result = eval(inp)  # builds string result from the evaluation of user input expression
-
-    if isinstance(result, float):
-        result = round(result, 3)
-
     return render_template('calc.html', result=result)  # sends result to page
 
 
