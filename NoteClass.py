76f9c94b19a0a3950fdb5ad8b519e29fe5435904
rakazirut <kaz.brah@gmail.com>
2018-10-25 16:51:37 -0400

    Better Music Player

--- NoteClass.py
+++ NoteClass.py
@@ -4,7 +4,7 @@ from io import open
 class NoteClass(object):
     @staticmethod
     def saveNote(file, data):
-        f = open(file, "w", newline='')
+        f = open('/Users/rakaz/Documents/GitHub/SwissArmyAssistant/static/media/Documents/'+file, "w", newline='')
         f.write("%s" % data)
         f.close()
         return
@@ -12,12 +12,12 @@ class NoteClass(object):
     @staticmethod
     def loadNote(file):
         try:  # crash proofing if file does not exist
-            open(file, "r", newline='')
+            open('/Users/rakaz/Documents/GitHub/SwissArmyAssistant/static/media/Documents/'+file, "r", newline='')
         except FileNotFoundError:
             data = "file not found!"
             return data
 
-        f = open(file, "r", newline='')
+        f = open('/Users/rakaz/Documents/GitHub/SwissArmyAssistant/static/media/Documents/'+file, "r", newline='')
         data = "null"
 
         if f.mode == 'r':
