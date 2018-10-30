800a96d23a7b2b64ebf156ac50698b7f30552052
connor <dalek124@gmail.com>
2018-10-30 11:00:04 -0400

    Log actually f*king works

--- log.py
+++ log.py
@@ -1,10 +1,18 @@
 from datetime import datetime
-from file import setFilePath
+import os
 
+
+def setFilePath():
+    # Get the root of this python file
+    root = os.path.dirname(os.path.abspath(__file__))
+    path = root + '/log/debug.log'
+
+    return path
+
+logLocation = setFilePath()
 
 def writeLog(logText):
     stamp = datetime.now()
-    logLocation = setFilePath('log')
     log = open(logLocation, "a")
     log.write(str(stamp) + " - " + str(logText))
     log.write("\n")
